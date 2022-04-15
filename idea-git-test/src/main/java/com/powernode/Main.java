1c8493ed0d61ece2c5d83339f2e4512c77f6f382
zl <2205741729@qq.com>
2022-04-15 20:10:23 +0800

    将项目提交到本地仓库

--- idea-git-test/src/main/java/com/powernode/Main.java
+++ idea-git-test/src/main/java/com/powernode/Main.java
@@ -3,6 +3,6 @@ package com.powernode;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello java");
+        System.out.println("hello git");
     }
 }
